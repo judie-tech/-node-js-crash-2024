54b03763bb16d53dbb7726ab0cd155725aa9379d
Judy <ritchiejudie691@gmail.com>
2024-09-10 21:05:59 +0300

    postman

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-import { log } from "console";
 import http from "http";
-const PORT = process.env.PORT;
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const PORT = process.env.PORT || 8000; // Default to 8000 if PORT is undefined
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World!!!</h1>");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Homepage!</h1>");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>About</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>Not Found</h1>");
+  }
 });
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
