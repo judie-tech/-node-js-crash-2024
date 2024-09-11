b86266f0693a195c9cfc301386954b2746854807
Judy <ritchiejudie691@gmail.com>
2024-09-11 15:55:11 +0300

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 import http from "http";
 import dotenv from "dotenv";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url"; // Import the fileURLToPath function from the 'url' module
 
 // Load environment variables from .env file
 dotenv.config();
 
 const PORT = process.env.PORT || 8000; // Default to 8000 if PORT is undefined
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Homepage!</h1>");
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>About</h1>");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>Not Found</h1>");
+// Get current path
+const __filename = fileURLToPath(import.meta.url); // Use fileURLToPath from the 'url' module
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Not Found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
   }
 });
 
---
+++ server2.js
@@ -0,0 +1,16 @@
+import { resolve } from "dns/promises";
+import { createServer } from "http";
+const PORT = process.env.PORT;
+
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
+  { id: 3, name: "Jim Doe" },
+];
+const server = createServer((req, res) => {
+  if (req.url === "api/users" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(users));
+    res.end();
+  }
+});
